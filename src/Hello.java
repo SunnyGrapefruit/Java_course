f2facf62e00077df6dacb0ab5c78e7f2eafc1084
SunnyGrapefruit <wir_die_traumer@mail.ru>
2020-02-25 23:26:09 +0500

    Hello World!

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,7 @@
 import java.security.MessageDigest;
 
 public class Hello {
-
-    public static void main(String[] args) throws Exception {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
-        for (byte b : digest) {
-            System.out.printf("%02x", b);
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
